ba4b77e35a30a6afc56acac7ec477bc8499292a2
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-03-16 17:44:44 -0400

    commit tree b3750e1e936aeb72e47b7ef94f5030fb2aecd92d

---
+++ lib/song.rb
@@ -0,0 +1,37 @@
+require 'pry'
+
+class Song
+    @@count = 0
+    @@artists = []
+    @@genres = []
+    attr_reader :artist, :name, :genre
+
+    def initialize (name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
+    end
+
+    def self.count
+        @@count
+    end
+    def self.artists
+        @@artists.uniq
+    end
+    def self.genres
+        @@genres.uniq
+    end
+    def self.artist_count
+        @@artists.tally
+    end
+    def self.genre_count
+        @@genres.tally
+    end
+end
+
+sing = Song.new("singing", "molly", "da best")
+#binding.pry
+0
